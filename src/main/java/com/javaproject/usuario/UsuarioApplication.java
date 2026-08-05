781c06e9fa960585175d855031e9ea6f4986bf62
Ferreira <107485754+Ferreiraah@users.noreply.github.com>
2026-08-05 11:25:07 -0300

    Revert "Feature/atualizacoes"

--- src/main/java/com/javaproject/usuario/UsuarioApplication.java
+++ src/main/java/com/javaproject/usuario/UsuarioApplication.java
@@ -1,4 +1,4 @@
-package com.javaproject;
+package com.javaproject.usuario;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
